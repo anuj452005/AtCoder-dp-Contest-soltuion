47dfd3ec8baa28f9f0246a2786601a39ac497193
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-07-30 19:05:46 +0530

    C_Vacation.cpp

---
+++ C_Vacation.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl "\n"
+#define ll int long long 
+void solve(){
+    
+}
+
+signed  main(){
+    fast
+int t;
+cin>>t;
+while(t--){
+solve();
+
+
+}
+   return 0;
+}
